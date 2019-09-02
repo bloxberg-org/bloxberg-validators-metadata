25339cbb4627e5151817397043bfa6eb35e6cced
James <crossoveranx@gmail.com>
2019-09-02 15:13:42 +0200

    update manifest

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -3,7 +3,7 @@ messages.wrongRepo = function(repo) {
   return `There is no such file in configured repo ${repo}`
 }
 messages.invalidaVotingKey =
-  'something went wrong is not a valid voting Key or you are connected to the wrong chain! Please make sure you have loaded the correct voting key in Metamask / Nifty Wallet.'
+  'The key is not a valid voting Key or you are connected to the wrong chain! Please make sure you have loaded the correct voting key in Metamask / Nifty Wallet.'
 messages.noMetamaskAccount = `Your MetaMask is locked.
 Please choose your voting key in MetaMask and reload the page.
 `
